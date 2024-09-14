e97059a85d32dbe0c0939391b88f2f06c5bb4232
Julli Mayanne <jullimayanne9@gmail.com>
2024-09-14 04:26:46 -0300

    feat: create new fololder

--- lib/core/network/adapter/network_adapter.dart
+++ lib/core/network/adapter/network_adapter.dart
@@ -3,40 +3,40 @@ import 'dart:async';
 abstract class HttpClient {
   set baseUrl(String baseUrl);
 
-  Future<T?> get<T>(
-    String path, {
+  Future<T?> get<T>({
+    required String path,
     Map<String, dynamic>? params,
     Map<String, String>? headers,
   });
 
-  Future<T?> post<T>(
-    String path, {
+  Future<T?> post<T>({
+    required String path,
     dynamic data,
     bool needAuth = true,
     Map<String, String>? headers,
   });
 
-  Future<T?> put<T>(
-    String path, {
+  Future<T?> put<T>({
+    required String path,
     dynamic data,
     Map<String, String>? headers,
   });
 
-  Future<T?> delete<T>(
-    String path, {
+  Future<T?> delete<T>({
+    required String path,
     Map<String, dynamic>? params,
     Map<String, String>? headers,
   });
 
-  Future<T?> patch<T>(
-    String path, {
+  Future<T?> patch<T>({
+    required String path,
     dynamic data,
     Map<String, String>? headers,
   });
 
-  Future<T?> request<T>(
-    String path,
-    String method, {
+  Future<T?> request<T>({
+    required String path,
+    required String method,
     dynamic data,
     Map<String, dynamic>? params,
     Map<String, String>? headers,
