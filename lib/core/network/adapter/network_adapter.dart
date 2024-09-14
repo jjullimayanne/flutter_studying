4e220493332e7fb7263afea4b6f0848cdd73954d
Julli Mayanne <jullimayanne9@gmail.com>
2024-09-14 04:23:48 -0300

    feat: create new fololder

--- lib/core/network/adapter/network_adapter.dart
+++ lib/core/network/adapter/network_adapter.dart
@@ -1,18 +1,17 @@
 import 'dart:async';
 
 abstract class HttpClient {
+  set baseUrl(String baseUrl);
+
   Future<T?> get<T>(
     String path, {
     Map<String, dynamic>? params,
-    bool hideInfoTracking = false,
     Map<String, String>? headers,
   });
 
   Future<T?> post<T>(
     String path, {
     dynamic data,
-    bool hideInfoTracking = false,
-    Function(int, int)? onSendProgress,
     bool needAuth = true,
     Map<String, String>? headers,
   });
@@ -20,23 +19,18 @@ abstract class HttpClient {
   Future<T?> put<T>(
     String path, {
     dynamic data,
-    bool hideInfoTracking = false,
     Map<String, String>? headers,
   });
 
   Future<T?> delete<T>(
     String path, {
     Map<String, dynamic>? params,
-    bool hideInfoTracking = false,
     Map<String, String>? headers,
   });
 
   Future<T?> patch<T>(
     String path, {
     dynamic data,
-    bool hideInfoTracking = false,
-    Function(int, int)? onSendProgress,
-    Function(int, int)? onReceiveProgress,
     Map<String, String>? headers,
   });
 
@@ -45,14 +39,7 @@ abstract class HttpClient {
     String method, {
     dynamic data,
     Map<String, dynamic>? params,
-    bool hideInfoTracking = false,
-    Function(int, int)? onSendProgress,
-    Function(int, int)? onReceiveProgress,
     Map<String, String>? headers,
     bool needAuth = true,
   });
-
-  set baseUrl(String baseUrl);
-
-  void addDefaultHeader(String key, String value);
 }
